4c86016634dfe7fdb5246d91409d9b3763e7b867
truongtnhe170291 <truongtnhe170291@fpt.edu.vn>
2024-05-16 22:34:14 +0700

    Update Code

---
+++ src/java/services/CVService.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package services;
+
+import dal.CVDAO;
+import models.CV;
+
+/**
+ *
+ * @author Admin
+ */
+public class CVService {
+    private static final CVService cvService = new CVService();
+    private final CVDAO cvDao;
+    
+    public static CVService getInstance(){
+        return cvService;
+    }
+
+    private CVService() {
+        cvDao = new CVDAO();
+    }
+    
+    public boolean createCV(CV cv){
+        return cvDao.addCV(cv);
+    }
+}
