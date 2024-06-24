3f96d06f1cc7bc517c7cfcbd73e389cab38ae8ae
Pham Son <sonphhe173195@fpt.edu.vn>
2024-06-24 11:34:43 +0700

    saving pprocess

--- src/java/controller/manager/HandleRequestMentor.java
+++ src/java/controller/manager/HandleRequestMentor.java
@@ -69,6 +69,7 @@ public class HandleRequestMentor extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
+      
 
         ScheduleDAO scheduleDAO = new ScheduleDAO();
 
